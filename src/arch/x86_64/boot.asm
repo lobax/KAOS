c1ba2802c46c11ba77acb33e5bee2417e334bed0
Kristian <krijor@kth.se>
2016-11-12 13:01:14 +0100

    Added stack & error function

--- src/arch/x86_64/boot.asm
+++ src/arch/x86_64/boot.asm
@@ -2,6 +2,18 @@ global start
 section .text
 bits 32
 start: 
+    mov esp, stack_top
     ; print `OK` to scree
     mov dword [0xb8000], 0x2f4b2f4f
     hlt
+
+error: 
+    mov dword [0xb8000], 0x4f524f45
+    mov dword [0xb8004], 0x4f3a4f52
+    mov dword [0xb8008], 0x4f204f20
+    mov byte  [0xb800a], al
+    hlt
+section .bss
+stack_bottom: 
+    resb 64
+stack_top: 
