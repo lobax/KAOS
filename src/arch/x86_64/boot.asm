b417a9df390ee2b1db408b9d6d49b7e2d26df0f6
Kristian <krijor@kth.se>
2016-11-12 13:18:25 +0100

    Added a function that checks for multiboot

--- src/arch/x86_64/boot.asm
+++ src/arch/x86_64/boot.asm
@@ -7,12 +7,24 @@ start:
     mov dword [0xb8000], 0x2f4b2f4f
     hlt
 
+; Prints 'ERR: ' and the given error code to the screen. 
+; Parameter: error code (in ascii) in al
 error: 
     mov dword [0xb8000], 0x4f524f45
     mov dword [0xb8004], 0x4f3a4f52
     mov dword [0xb8008], 0x4f204f20
     mov byte  [0xb800a], al
     hlt
+
+; Throw error 0 if eax doesn't contain the Multiboot 2 magic value (0x36d76289) 
+check_multiboot: 
+    cmp eax, 0x36d76289
+    jne .no_multiboot
+    ret
+.no_multiboot: 
+    mov al, "0"
+    jmp error
+
 section .bss
 stack_bottom: 
     resb 64
