9b9da8ce4dc58a7df889329069d5047bbb681f0b
Kristian <krijor@kth.se>
2016-11-12 13:52:07 +0100

    Added check for long mode

--- src/arch/x86_64/boot.asm
+++ src/arch/x86_64/boot.asm
@@ -25,6 +25,60 @@ check_multiboot:
     mov al, "0"
     jmp error
 
+check_cpuid: 
+    ; Check if CPUID is supported by attempting to flip the ID bit (bit 21)
+    ; in the FLAGS register. IF we can flip it, CPUID is available. 
+
+    ; Copy FLAGS in to EAX via stack 
+    pushfd
+    pop eax
+
+    ; Copy to ECX as well for comparing later on
+    mov ecx, eax
+
+    ; Flip the ID bit
+    xor eax, 1 << 21
+
+    ; Copy EAX to FLAGS via the stack
+    push eax
+    popfd
+
+    ; Copy FLAGS back to EAX (with the flipped bit if CPUID is supported) 
+    pushfd
+    pop eax
+
+    ; Restore FLAGS from the old version 
+    ; (flipping the ID bit back if it was ever supported)
+    push ecx
+    popfd
+
+    ; Compare EAX and ECX. If they are equal then that means the bit
+    ; wasn't flipped, and CPUID isn't supported 
+    cmp eax, ecx
+    je .no_cupid
+    ret
+.no_cpuid: 
+    mov al, "1"
+    jmp error
+
+; Throw error 2 if the CPU doesn't support Long Mode. 
+check_long_mode: 
+    ; test if extended processor info is available
+    mov eax, 0x80000000     ; implicit argument for cpuid
+    cpuid                   ; get highest supported argument
+    cmp eax, 0x80000001     ; it needs to be at least 0x80000001
+    jb .no_long_mode        ; if it is less, the CPU is to old
+
+    ; use extended info to test if long mode is available
+    mov eax, 0x80000001     ; argument for extended processor info
+    cpuid                   ; returns various feature bits in ecx and edx
+    test edx, 1 << 29       ; test if the LM-bit is set in the D-register
+    jz .no_long_mode        ; If it's not set, there is no long mode
+    ret
+.no_long_mode: 
+    mov al, "2"
+    jmp error
+
 section .bss
 stack_bottom: 
     resb 64
